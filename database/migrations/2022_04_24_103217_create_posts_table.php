1fce26983a6458dfd2d937eeb1809ead9b36c85e
nghia23d <nghia23d@gmail.com>
2022-05-02 16:40:53 +0700

    init

---
+++ database/migrations/2022_04_24_103217_create_posts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('lead');
+            $table->text('content');
+
+            //topic
+            $table->unsignedBigInteger('topic_id')->index()->nullable();
+            $table->foreign('topic_id')->references('id')->on('topis');
+
+            //user
+            $table->unsignedBigInteger('author_id')->index()->nullable();
+            $table->foreign('author_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
